9613df254ace96df14cfdcc87b4303a1d2ed0aa2
tanvirch0750 <tanvir0750@yahoo.com>
2022-07-23 20:57:35 +0600

    implemented signin functionality

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 
-import { signup } from '../controller/user.js';
+import { signin, signup } from '../controller/user.js';
 
 router.post('/signup', signup);
+router.post('/signin', signin);
 
 export default router;
