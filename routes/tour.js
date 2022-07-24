df91cbf8694ce220588ac58e705926582d422795
tanvirch0750 <tanvir0750@yahoo.com>
2022-07-24 23:21:02 +0600

    implemented delete and update controller and route

--- routes/tour.js
+++ routes/tour.js
@@ -4,14 +4,18 @@ const router = express.Router();
 
 import {
   createTour,
+  deleteTour,
   getTour,
   getTours,
   getToursByUser,
+  updateTour,
 } from '../controller/tour.js';
 
 router.post('/', auth, createTour);
 router.get('/', getTours);
 router.get('/:id', getTour);
+router.delete('/:id', auth, deleteTour);
+router.patch('/:id', auth, updateTour);
 router.get('/userTours/:id', auth, getToursByUser);
 
 export default router;
